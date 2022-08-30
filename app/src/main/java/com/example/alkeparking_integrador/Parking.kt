580a43064774229fb01c17534b91be0980728473
David <david.salazar@mercadolibre.com>
2022-08-30 15:06:39 -0300

    Clean file Parking.kt

--- app/src/main/java/com/example/alkeparking_integrador/Parking.kt
+++ app/src/main/java/com/example/alkeparking_integrador/Parking.kt
@@ -1,17 +1,5 @@
 package com.example.alkeparking_integrador
 
-data class ParkingSpace(var vehicle: Vehicle)
-
 data class Parking(val vehicles: MutableSet<String>) {
 }
 
-data class Vehicle(val plate: String) {
-    override fun equals(other: Any?): Boolean {
-        if (other is Vehicle) {
-            return this.plate == other.plate
-        }
-        return super.equals(other)
-    }
-
-    override fun hashCode(): Int = this.plate.hashCode()
-}
