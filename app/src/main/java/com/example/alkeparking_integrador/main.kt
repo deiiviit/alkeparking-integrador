74b0dfcff94aa41796f38de894429725f4565141
David <david.salazar@mercadolibre.com>
2022-08-30 17:02:46 -0300

    Main casos de pruebas

--- app/src/main/java/com/example/alkeparking_integrador/main.kt
+++ app/src/main/java/com/example/alkeparking_integrador/main.kt
@@ -1,9 +1,26 @@
 package com.example.alkeparking_integrador
 
-import com.example.alkeparking_integrador.VehicleType.*
 import java.util.*
 
 fun main() {
     val car = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_001")
-    val moto = Vehicle("AA111AA", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val moto = Vehicle("BA111AA", VehicleType.MOTORCYCLE, Calendar.getInstance())
+    val minibus = Vehicle("CA111AA", VehicleType.MINIBUS, Calendar.getInstance(), "DISCOUNT_CARD_002")
+    val bus = Vehicle("DA111AA", VehicleType.BUS, Calendar.getInstance())
+    val parking = Parking(mutableSetOf(car,moto,minibus,bus))
+
+//    val busCopy = Vehicle("DA111AA", VehicleType.BUS, Calendar.getInstance())
+//    val bus2 = Vehicle("FA111AA", VehicleType.BUS, Calendar.getInstance())
+
+
+    println(parking.vehicles.contains(car))
+    println(parking.vehicles.contains(moto))
+    println(parking.vehicles.contains(minibus))
+    println(parking.vehicles.contains(bus))
+
+
+//    println(parking.vehicles.contains(busCopy))
+//    println(parking.vehicles.size)
+//    println(parking.vehicles.contains(bus2))
+
 }
