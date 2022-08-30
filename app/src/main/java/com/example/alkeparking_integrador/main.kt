efbdbc6ed6a0f15c4160680f752164816f9bb11a
David <david.salazar@mercadolibre.com>
2022-08-30 16:26:08 -0300

    File main created

---
+++ app/src/main/java/com/example/alkeparking_integrador/main.kt
@@ -0,0 +1,9 @@
+package com.example.alkeparking_integrador
+
+import com.example.alkeparking_integrador.VehicleType.*
+import java.util.*
+
+fun main() {
+    val car = Vehicle("AA111AA", VehicleType.CAR, Calendar.getInstance(), "DISCOUNT_CARD_001")
+    val moto = Vehicle("AA111AA", VehicleType.MOTORCYCLE, Calendar.getInstance())
+}
