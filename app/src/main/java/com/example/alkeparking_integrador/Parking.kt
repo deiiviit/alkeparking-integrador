d18998bc8ef6fe5801b572422765601503b16203
David <david.salazar@mercadolibre.com>
2022-08-30 14:37:54 -0300

    Initial code given

--- app/src/main/java/com/example/alkeparking_integrador/Parking.kt
+++ app/src/main/java/com/example/alkeparking_integrador/Parking.kt
@@ -1,5 +1,17 @@
 package com.example.alkeparking_integrador
 
+data class ParkingSpace(var vehicle: Vehicle)
+
 data class Parking(val vehicles: MutableSet<String>) {
-// commit
+}
+
+data class Vehicle(val plate: String) {
+    override fun equals(other: Any?): Boolean {
+        if (other is Vehicle) {
+            return this.plate == other.plate
+        }
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int = this.plate.hashCode()
 }
