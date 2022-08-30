e7857e043c11493a6fd206b2111fde31adad2d71
sdepetris <stefano.depetris@mercadolibre.com>
2022-08-30 16:45:30 -0300

    change name parameter

--- app/src/main/java/com/example/alkeparking_integrador/Vehicle.kt
+++ app/src/main/java/com/example/alkeparking_integrador/Vehicle.kt
@@ -2,8 +2,8 @@ package com.example.alkeparking_integrador
 
 import java.util.*
 
-data class Vehicle(val plate:String, val type: VehicleType, val instance: Calendar,val card:String?=null){
-    
+data class Vehicle(val plate:String, val type: VehicleType, val checkInTime: Calendar,val discountCard:String?=null){
+
     override fun equals(other: Any?): Boolean {
         if(other is Vehicle){
             return this.plate==other.plate
