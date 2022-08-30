d1771aa0a023c91619acbc00161e09c6b255427a
sdepetris <stefano.depetris@mercadolibre.com>
2022-08-30 16:15:18 -0300

    Add parameters in vehicle

--- app/src/main/java/com/example/alkeparking_integrador/Vehicle.kt
+++ app/src/main/java/com/example/alkeparking_integrador/Vehicle.kt
@@ -1,7 +1,9 @@
 package com.example.alkeparking_integrador
 
-data class Vehicle(val plate:String){
+import java.util.*
 
+data class Vehicle(val plate:String, val type: VehicleType, val instance: Calendar,val card:String?=null){
+    
     override fun equals(other: Any?): Boolean {
         if(other is Vehicle){
             return this.plate==other.plate
