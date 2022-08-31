3a0f7037549cf55bf53764fa1c9ae2148a8659dc
tbonillad <tatiana.bonilla@mercadolibre.com.co>
2022-08-31 02:01:07 -0500

    Se implementa funcion checkIn y sus validaciones (placa repetida y cupo maximo)

--- app/src/main/java/com/example/alkeparking_integrador/Parking.kt
+++ app/src/main/java/com/example/alkeparking_integrador/Parking.kt
@@ -2,6 +2,30 @@ package com.example.alkeparking_integrador
 
 // El set admites unicos, no pueden ver 2 vehiculos en el la misma plate
 
-data class Parking(val vehicles: MutableSet<Vehicle>) {
+const val MAXIMUM_CAPACITY: Int = 20
+
+data class Parking(var vehicles: MutableSet<Vehicle>) {
+
+    /**
+     * Metodo encargado de la validacion de la placa y del llamado del método añadir vehiculo
+     */
+    fun checkIn(vehicle: Vehicle): String {
+        var filteredVehicles = vehicles.filter { it == vehicle }
+        if (filteredVehicles.isNotEmpty()) {
+            return "Sorry, the has check-in failed"
+        }
+        return if (addVehicle(vehicle)) "Welcome to AlkeParking!" else "Sorry, the check-in failed"
+    }
+
+    /**
+     * Metodo encargado de añadir vehiculo al mutableSet de vehiculo
+     */
+    private fun addVehicle(vehicle: Vehicle): Boolean {
+        if (vehicles.size < MAXIMUM_CAPACITY) {
+            vehicles.add(vehicle)
+            return true
+        }
+        return false
+    }
 }
 
